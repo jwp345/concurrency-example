e1beb02c57bb121e9d3ba8eab6a9e3a845789326
jwp345 <jwp135@naver.com>
2022-11-28 21:06:01 +0900

    네임드락(분산락)을 이용한 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,6 +15,7 @@ public class StockService {
     this.stockRepository = stockRepository;
   }
 
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
   public synchronized void decrease(Long id, Long quantity) {
     // get stock
     // 재고 감소
