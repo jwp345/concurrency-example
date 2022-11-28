5e19e125e568e070be8655cc0bbe7ae83ad22db6
jwp345 <jwp135@naver.com>
2022-11-28 20:38:09 +0900

    낙관적 락을 이용한 해결

--- src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
+++ src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -1,7 +1,8 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import com.example.stock.service.PessimisticLockStockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,10 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
-public class StockServiceTest {
+public class OptimisticLockStockFacadeTest {
 
   @Autowired
-  private PessimisticLockStockService stockService;
+  private OptimisticLockStockFacade optimisticLockStockFacade;
 
   @Autowired
   private StockRepository stockRepository;
@@ -37,17 +38,6 @@ public class StockServiceTest {
   }
 
   @Test
-  public void stock_decrease() {
-    stockService.decrease(1L, 1L);
-
-    // 100 - 1 = 99
-
-    Stock stock = stockRepository.findById(1L).orElseThrow();
-
-    assertEquals(99, stock.getQuantity());
-  }
-
-  @Test
   public void 동시에_100개의_요청() throws InterruptedException {
     int threadCount = 100;
     ExecutorService executorService = Executors.newFixedThreadPool(32);
@@ -56,7 +46,9 @@ public class StockServiceTest {
     for (int i = 0; i < threadCount; i++) {
       executorService.submit(() -> {
         try {
-          stockService.decrease(1L, 1L);
+          optimisticLockStockFacade.decrease(1L, 1L);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
         } finally {
           latch.countDown();
         }
